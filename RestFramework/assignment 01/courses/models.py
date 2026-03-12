93c239630faff302b5d3e9a68eb1e73dbb3a020f
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-12 19:35:32 +0600

    I have some changes

--- RestFramework/assignment 01/courses/models.py
+++ RestFramework/assignment 01/courses/models.py
@@ -1,9 +1,2 @@
 from django.db import models
 
-class Addmission(models.Model):
-    name = models.CharField(max_length=150)
-    roll = models.IntegerField()
-    cgpa = models.FloatField()
-
-    def __str__(self):
-        return f"{self.name} ({self.roll})"
