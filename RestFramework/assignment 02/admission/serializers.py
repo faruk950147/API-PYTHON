92a56ee41054b0164931cd0f134d16fb84403112
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-13 20:24:09 +0600

    I have some changes

--- RestFramework/assignment 02/admission/serializers.py
+++ RestFramework/assignment 02/admission/serializers.py
@@ -5,3 +5,13 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+        
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.roll = validated_data.get('roll', instance.roll)
+        instance.cgpa = validated_data.get('cgpa', instance.cgpa)
+        instance.save()
+        return instance
--- RestFramework/assignment 03/info/serializers.py
+++ RestFramework/assignment 03/info/serializers.py
@@ -5,3 +5,17 @@ class AdmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admission
         fields = '__all__'
+        
+    def create(self, validated_data):
+        return Admission.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.dob = validated_data.get('dob', instance.dob)
+        instance.gpa = validated_data.get('gpa', instance.gpa)
+        instance.qualification = validated_data.get('qualification', instance.qualification)
+        instance.gender = validated_data.get('gender', instance.gender)
+        instance.department = validated_data.get('department', instance.department)
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
+        return instance
