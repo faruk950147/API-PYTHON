968c0293b5c2084f99b821320add613a652a831b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-17 16:09:22 +0600

    I have some changes

--- RestFramework/assignment 06/office/serializers.py
+++ RestFramework/assignment 06/office/serializers.py
@@ -1,28 +1,5 @@
 from rest_framework import serializers
 from office.models import Company, Department, Employee
-"""
-# Company Serializer
-class CompanySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = "__all__"
-
-# Department Serializer
-class DepartmentSerializer(serializers.ModelSerializer):
-    company = serializers.StringRelatedField()
-    class Meta:
-        model = Department
-        fields = "__all__"
-
-# Employee Serializer
-class EmployeeSerializer(serializers.ModelSerializer):
-    company = serializers.StringRelatedField()
-    department = serializers.StringRelatedField()
-    
-    class Meta:
-        model = Employee
-        fields = "__all__" """
-        
         
 # Company Serializer
 class CompanyHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
