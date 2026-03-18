d32fa396d04342dbccbf37eed05b35d82a146794
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-18 18:55:09 +0600

    I have some changes

--- RestFramework/assignment 08/office/admin.py
+++ RestFramework/assignment 08/office/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from office.models import Employee
 
-# Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'department', 'dob', 'created_at', 'updated_at')
+    search_fields = ('name', 'email', 'department')
+    list_filter = ('department', 'created_at')
+    ordering = ('-created_at',)
