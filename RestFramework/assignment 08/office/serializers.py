ffd67822e17d17f11801ad86607acab16e66a1ab
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-19 18:36:04 +0600

    I have some changes

--- RestFramework/assignment 08/office/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from office.models import Employee
-
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = '__all__'
