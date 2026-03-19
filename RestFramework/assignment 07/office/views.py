3127aee78e4f9df26de1034ddb247293ab4b48ba
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-19 15:28:43 +0600

    I have some changes

--- RestFramework/assignment 07/office/views.py
+++ RestFramework/assignment 07/office/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import status
@@ -21,3 +21,48 @@ class APIRoot(APIView):
             # "departments": reverse("department-list", request=request, format=format),
             # "employees": reverse("employee-list", request=request, format=format),
         })
+        
+class CompanyMixins(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    generics.GenericAPIView):
+    """
+    CompanyMixins provides API endpoints for listing and creating companies.
+
+    This class uses DRF mixins to implement:
+        - Listing all companies (GET request)
+        - Creating a new company (POST request)
+
+    Attributes:
+        queryset (QuerySet): All Company objects from the database.
+        serializer_class (Serializer): Serializer used to convert Company objects
+            to JSON and validate incoming data.
+
+    Methods:
+        get(request, *args, **kwargs):
+            Handles GET requests to return a list of all companies.
+        post(request, *args, **kwargs):
+            Handles POST requests to create a new company.
+    """
+
+    queryset = Company.objects.all()
+    serializer_class = CompanyHyperlinkedSerializer
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests.
+
+        Returns a list of all companies serialized using
+        CompanyHyperlinkedSerializer.
+        """
+        return self.list(request, *args, **kwargs)  # from ListModelMixin
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests.
+
+        Creates a new company instance using the request data,
+        validates it with CompanyHyperlinkedSerializer, and
+        returns the serialized new object if successful.
+        """
+        return self.create(request, *args, **kwargs)  # from CreateModelMixin
+
