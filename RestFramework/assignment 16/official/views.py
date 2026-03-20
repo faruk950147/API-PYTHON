29efcf183a0945c7e263637c5086430bafc03438
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-20 21:04:33 +0600

    I have some changes

--- RestFramework/assignment 16/official/views.py
+++ RestFramework/assignment 16/official/views.py
@@ -3,44 +3,4 @@ from official.models import Post
 from official.serializers import PostSerializer
 
 # CreateAPIView
-class PostCreateAPIView(generics.CreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def perform_create(self, serializer):
-        # new post author automatically assign
-        serializer.save(author=self.request.user)
-
-
-# ListAPIView
-class PostListAPIView(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-# RetrieveAPIView
-class PostRetrieveAPIView(generics.RetrieveAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-# UpdateAPIView
-class PostUpdateAPIView(generics.UpdateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def perform_update(self, serializer):
-        # Optional: extra update logic
-        serializer.save()
-
-
-# DestroyAPIView
-class PostDestroyAPIView(generics.DestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def perform_destroy(self, instance):
-        # Optional: soft delete
-        # instance.is_deleted = True
-        # instance.save()
-        instance.delete()
+class PostAPIView(generics.CreateAPIView):
