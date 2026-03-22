c64e0c12dc31f66af511884e3255cfa7cbd512ee
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-22 12:45:04 +0600

    I have some changes

--- RestFramework/assignment 10/official/urls.py
+++ RestFramework/assignment 10/official/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from official.views import (
+    PostAPIView   
 )
 
 urlpatterns = [
-   
+   path('', PostAPIView.as_view())
 ]
--- RestFramework/assignment 10/official/views.py
+++ RestFramework/assignment 10/official/views.py
@@ -1,5 +1,63 @@
-from rest_framework import generics
+# Django built-in response class
+from django.http import HttpResponse
+
+# DRF core classes
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
+
+# Your Model and Serializer
 from official.models import Post
 from official.serializers import PostSerializer
 
-# CreateAPIView
+
+class PostAPIView(APIView):
+    def get(self, request):
+        # Fetch all posts from the database
+        posts = Post.objects.all()
+
+        # Debug: print queryset in console
+        print(f'Queryset: {posts}')
+
+        # Convert queryset to Python dict using serializer
+        serializer = PostSerializer(posts, many=True)
+
+        # Debug: print serialized data
+        print(f'Serialized Data: {serializer.data}')
+
+        # Convert Python dict to JSON
+        json_data = JSONRenderer().render(serializer.data)
+
+        # Return JSON response to client
+        return HttpResponse(
+            json_data,
+            content_type='application/json'
+        )
+    def post(self, request):
+
+        # Get incoming data from client
+        serializer = PostSerializer(data=request.data)
+
+        # Validate the data
+        if serializer.is_valid():
+
+            # Save valid data to database
+            serializer.save()
+
+            # Convert saved data to JSON
+            json_data = JSONRenderer().render(serializer.data)
+
+            # Return success response (201 Created)
+            return HttpResponse(
+                json_data,
+                content_type='application/json',
+                status=201
+            )
+
+        # If validation fails, return errors
+        json_data = JSONRenderer().render(serializer.errors)
+
+        return HttpResponse(
+            json_data,
+            content_type='application/json',
+            status=400
+        )
