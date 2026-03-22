6bda486439fbfb5cf92173bba19b7a991cf82aff
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-22 12:07:47 +0600

    I have some changes

--- RestFramework/assignment 11/official/urls.py
+++ RestFramework/assignment 11/official/urls.py
@@ -1,25 +1,7 @@
 from django.urls import path
 from official.views import (
-    PostCreateAPIView,
-    PostListAPIView,
-    PostRetrieveAPIView,
-    PostUpdateAPIView,
-    PostDestroyAPIView
 )
 
 urlpatterns = [
-    # List all posts
-    path('posts/', PostListAPIView.as_view(), name='post-list'),
-
-    # Create new post
-    path('posts/create/', PostCreateAPIView.as_view(), name='post-create'),
-
-    # Retrieve a single post
-    path('posts/<int:pk>/', PostRetrieveAPIView.as_view(), name='post-detail'),
-
-    # Update a post
-    path('posts/<int:pk>/update/', PostUpdateAPIView.as_view(), name='post-update'),
-
-    # Delete a post
-    path('posts/<int:pk>/delete/', PostDestroyAPIView.as_view(), name='post-delete'),
+   
 ]
