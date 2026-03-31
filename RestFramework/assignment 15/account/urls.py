2d513a41afbf1f613ce5e6812019c6ddda83b827
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-31 18:31:00 +0600

    I have some changes

--- RestFramework/assignment 15/account/urls.py
+++ RestFramework/assignment 15/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+# it is used to generate token for user authentication
+from rest_framework.authtoken.views import obtain_auth_token 
 
 urlpatterns = [  
     path('get-token/', obtain_auth_token, name='get-token'),
