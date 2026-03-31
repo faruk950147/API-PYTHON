acf5b61743bb729c393d6d19a35b0dddf4d8a4a6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-31 19:33:08 +0600

    I have some changes

--- RestFramework/assignment 16/account/views.py
+++ RestFramework/assignment 16/account/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.views import Token, obtain_auth_token 
 
 # Create your views here.
+
+class CustomAuthToken(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
