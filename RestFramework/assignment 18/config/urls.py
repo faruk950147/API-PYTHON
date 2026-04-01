e74200f9e6764c74adb2fcbd78fa39e09ea7554d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-01 21:08:29 +0600

    I have some changes

--- RestFramework/assignment 18/config/urls.py
+++ RestFramework/assignment 18/config/urls.py
@@ -10,7 +10,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', include('official.urls')),
-    path('api/account/', include('account.urls')),
+    path('account/', include('account.urls')),
     path('api/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
