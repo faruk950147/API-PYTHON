4e399634b595aa8ee952596059d3c473a6fd1ff4
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-20 14:26:18 +0600

    I have some changes

--- RestFramework/assignment 18/account/api_urls.py
+++ RestFramework/assignment 18/account/api_urls.py
@@ -1,14 +1,27 @@
 from django.urls import path
-from account.api_views import *
+from account.api_views import (
+    SignupView,
+    OTPVerifyView,
+    LoginView,
+    LogoutView,
+    ChangePasswordView,
+    ResetPasswordRequestView,
+    ResetPasswordOTPVerifyView,
+    SetPasswordView,
+    ResendOTPView
+)
 
 urlpatterns = [
-    path("signup/", SignupView.as_view()),
-    path("verify-account-otp/", OTPVerifyView.as_view()),
-    path("login/", LoginView.as_view()),
-    path("logout/", LogoutView.as_view()),
-    path("change-password/", ChangePasswordView.as_view()),
-    path("reset-password/", ResetPasswordRequestView.as_view()),
-    path("verify-reset-otp/", ResetPasswordOTPVerifyView.as_view()),
-    path("set-password/", SetPasswordView.as_view()),
-    path("resend-otp/", ResendOTPView.as_view()),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("verify-account-otp/", OTPVerifyView.as_view(), name="verify-account-otp"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+
+    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
+
+    path("reset-password/", ResetPasswordRequestView.as_view(), name="reset-password"),
+    path("verify-reset-otp/", ResetPasswordOTPVerifyView.as_view(), name="verify-reset-otp"),
+    path("set-password/", SetPasswordView.as_view(), name="set-password"),
+
+    path("resend-otp/", ResendOTPView.as_view(), name="resend-otp"),
 ]
