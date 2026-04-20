af106bdb5714a286a63a6da2f96e3542547ac337
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-20 12:17:55 +0600

    I have some changes

--- RestFramework/assignment 18/account/api_urls.py
+++ RestFramework/assignment 18/account/api_urls.py
@@ -3,11 +3,12 @@ from account.api_views import *
 
 urlpatterns = [
     path("signup/", SignupView.as_view()),
-    path("verify-otp/", OTPVerifyView.as_view()),
+    path("verify-account-otp/", OTPVerifyView.as_view()),
     path("login/", LoginView.as_view()),
     path("logout/", LogoutView.as_view()),
     path("change-password/", ChangePasswordView.as_view()),
-    path("reset-password-request/", ResetPasswordRequestView.as_view()),
-    path("reset-password-confirm/", ResetPasswordConfirmView.as_view()),
+    path("reset-password/", ResetPasswordRequestView.as_view()),
+    path("verify-reset-otp/", ResetPasswordOTPVerifyView.as_view()),
+    path("set-password/", SetPasswordView.as_view()),
     path("resend-otp/", ResendOTPView.as_view()),
 ]
