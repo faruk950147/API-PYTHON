23deab90a7262aff0ca10bf3ae5360d3aeae9680
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-07-31 20:33:14 +0600

    changes some files

--- RestFramework/assignment 18/account/api_urls.py
+++ RestFramework/assignment 18/account/api_urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from account.api_views import*
 
 urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("otp/verify/signup/", OTPVerifyView.as_view(), name="verify-account-otp"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", SignupView.as_view()),
+    path("otp/verify/signup/", OTPVerifyView.as_view()),
+    path("login/", LoginView.as_view()),
+    path("logout/", LogoutView.as_view()),
 
-    path("password/change/", ChangePasswordView.as_view(), name="change-password"),
+    path("password/change/", ChangePasswordView.as_view()),
 
-    path("password/reset/request/", ResetPasswordRequestView.as_view(), name="reset-password"),
-    path("otp/verify/reset/", ResetPasswordOTPVerifyView.as_view(), name="verify-reset-otp"),
-    path("password/reset/set/", SetPasswordView.as_view(), name="set-password"),
+    path("password/reset/request/", ResetPasswordRequestView.as_view()),
+    path("otp/verify/reset/", ResetPasswordOTPVerifyView.as_view()),
+    path("password/reset/set/", SetPasswordView.as_view()),
 
-    path("otp/resend/", ResendOTPView.as_view(), name="resend-otp"),
+    path("otp/resend/", ResendOTPView.as_view()),
 ]
